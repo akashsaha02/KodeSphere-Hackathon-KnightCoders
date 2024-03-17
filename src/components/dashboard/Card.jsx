45d5e3101499d8aef836cc98f9b4da61fb759c9b
Akash4056 <akashsaha1313@gmail.com>
2024-03-17 16:30:32 +0530

    card updated

--- src/components/dashboard/Card.jsx
+++ src/components/dashboard/Card.jsx
@@ -2,11 +2,14 @@ import MinCard from "./MinCard";
 
 const Card = ({ modalHandler, setHeading, setLogo }) => {
   return (
-    <div className="min-h-[20rem]  bg-gray-100 border-transparent shadow-xl rounded-xl">
+    <div className="min-h-[20rem]  bg-gray-200 border-transparent rounded-b-xl">
       <div className="pb-5">
         <h2 className="text-center text-3xl font-semibold pt-10">
           Control Devices
         </h2>
+        <div className="flex justify-center mt-2">
+        <hr className="border-2 border-black  w-52 rounded-full text-center" />
+        </div>
       </div>
       <div className="flex items-center justify-center gap-14">
         <div
